b44e0849169d001fc90bef6163f85d3be4f55671
egujito <bessapachecolr@gmail.com>
2025-12-05 16:12:00 +0000

    Added safety assert in Motor, so fuel masses cannot be negative

--- flightForge/motor.py
+++ flightForge/motor.py
@@ -10,6 +10,11 @@ class Motor:
         if mass_ot is not None:
             self.mass_curve = func_from_csv(mass_ot)
 
+        self._assert_flow_rates()
+
+    def _assert_flow_rates(self):
+        if self.burn_time*self.fuel_mdot > self.fuel_load:
+            raise Exception(f"\n !Tank will be underfilled. \n burn_time * fuel_mdot = {self.burn_time*self.fuel_mdot} kg \n fuel_load = {self.fuel_load} kg")
 
     def thrust(self, t):
         return self.thrust_curve(t)
