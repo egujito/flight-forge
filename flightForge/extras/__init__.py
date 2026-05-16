370b8e2fe1e4592da4ec4ec12ca8e1d9cbc98eea
egujito <bessapachecolr@gmail.com>
2026-05-16 01:58:57 +0100

    feat(extras): add RunSpec, deep-path helpers, and isolated executor

--- flightForge/extras/__init__.py
+++ flightForge/extras/__init__.py
@@ -1,9 +1,15 @@
 from .param import FixedParam, Param, ParamLike, StochasticParam, SweepParam
+from .runner import BaseObjects, RunSpec, deep_get, deep_set, execute_run
 
 __all__ = [
+    "BaseObjects",
     "FixedParam",
     "Param",
     "ParamLike",
+    "RunSpec",
     "StochasticParam",
     "SweepParam",
+    "deep_get",
+    "deep_set",
+    "execute_run",
 ]
